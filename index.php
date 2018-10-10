4e46cd9eddcda398649a835536188d2a821c6169
Tasmia Noor <tasmia.noor@gmail.com>
2018-10-09 20:35:13 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+         
+        <title>The Hogwarts Quiz</title>
+        
+        <link rel="stylesheet" href="style.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+    
+    </head>
+    
+    <body>
+        
+        <div class="container">
+            <h1>What's your Hogwarts house?</h1>
+                <img src="images/photo.png" alt="Picture of Tasmia Noor">
+
+            <p>Which Hogwarts house will you be sorted into? Gryffindor, Hufflepuff, Ravenclaw or Slytherin? Answer these 
+               questions and let us be your Sorting Hat!</p> 
