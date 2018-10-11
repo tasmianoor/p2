df4902ac1dc63b66c79152704ffedfab48386bcd
Tasmia Noor <tasmia.noor@gmail.com>
2018-10-10 22:18:34 -0400

    Updated form.

--- index.php
+++ index.php
@@ -22,3 +22,21 @@
 
             <p>Which Hogwarts house will you be sorted into? Gryffindor, Hufflepuff, Ravenclaw or Slytherin? Answer these 
                questions and let us be your Sorting Hat!</p> 
+            
+        <form action="index_get.php" method="post">
+            First 3 letters of your name: <input type="text" name="name"><br>
+            
+            Select your favorite color:
+                <input type="radio" name="blue" value="blue">Blue
+                <input type="radio" name="green" value="green">Green
+                <input type="radio" name="red" value="red">Red
+                <input type="radio" name="red" value="red">Red
+
+            
+
+            
+            
+            
+            <input type="submit">
+            
+        </form>
