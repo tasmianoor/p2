fb609fcfd18b240459a4bd66fd10c813e842128d
Tasmia Noor <tasmia.noor@gmail.com>
2018-10-11 19:47:23 -0400

    Changed h5 to h4

--- index.php
+++ index.php
@@ -21,8 +21,8 @@
             
             <img src="images/form_image.png" alt="Hogwarts shield">
 
-            <h5>Which Hogwarts house will you be sorted into? Gryffindor, Hufflepuff, Ravenclaw or Slytherin? Answer these 
-               questions and let us be your Sorting Hat!</h5> 
+            <h4>Which Hogwarts house will you be sorted into? Gryffindor, Hufflepuff, Ravenclaw or Slytherin? Answer these 
+               questions and let us be your Sorting Hat!</h4> 
             
         <form action="index_get.php" method="post">
             <p>First 3 letters of your name:</p> <input type="text" name="name"><br/>
