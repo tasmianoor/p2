78f75a25cccbbdf5df11b710a6b6352d44433ae1
Tasmia Noor <tasmia.noor@gmail.com>
2018-10-10 23:41:52 -0400

    Update index_get.php

--- index_get.php
+++ index_get.php
@@ -3,9 +3,38 @@
 
 Welcome to Hogwarts, <?php echo $_GET["name"]; ?>ius!<br>
  
-$color = $_GET['color'];
+<?php ini_set('display_errors',1);
+   $quotes=[
+        [
+            'title' => 'Gryfindor',
+             'image' => img src='/images/to/Gryfindor.jpg'.
+       [
+            'title' => 'Ravenclaw',
+            'image' => img src='/images/to/Ravenclaw.jpg'
+        ],  
+        [
+            'title' => 'Hufflepuff',
+            image' => img src='/images/to/Hufflepuff.jpg'
+        ],
+     [
+            'title' => 'Slytherin',
+            image' => img src='/images/Ravenclaw.jpg'
+        ],
+         [
+            'title' => 'Jackie Joyn, track and field athlete',
+            'image' => 'If I stop to kick every barking dog I am not going to get where I’m going.'
+        ]
+    ]; 
 
-echo 'Your house is...'.$color;    
+$quoteno = rand (1,3);
+
+ $quotesnow = $quotes[$quoteno-1];
+
+$quoteTitle = $quotesnow['title'];
+
+$quoteImage= $quotesnow['image'];
+
+?>  
     
     
 </body>
